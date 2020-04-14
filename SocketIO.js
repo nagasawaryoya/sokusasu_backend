fb2dd3a41b5ba6616355a741a04b2b35294e450e
aidma287 <aidma287@aidma287noMacBook-Pro.local>
2020-04-14 22:46:26 +0900

    SB-3 紛れ込んでたsocket.ioの修正部分コミット

--- SocketIO.js
+++ SocketIO.js
@@ -12,20 +12,19 @@ var io = require('socket.io')(server);
 
 //socketIOモジュール
 function socketIO(){
-    // //サーバーを立ち上げたら実行
-    server.listen(app.get('port'), function() {
-        console.log('3000番：listening!!!');
-    });
+  // //サーバーを立ち上げたら実行
+  server.listen(app.get('port'), function() {
+    console.log('3000番：listening!!!');
+  });
 
-    //socket処理を記載する
-    io.sockets.on('connection', function(socket) {
-        console.log(`a user connected[id:${ socket.id }]`)
-        socket.on('POST_MESSAGE', function(data) {
-          console.log(`posted[name:${ data.name },message:${ data.message }]`)
-          io.sockets.emit('MESSAGE', data)
-        });
-      })
-       
+  //socket処理を記載する
+  io.sockets.on('connection', function(socket) {
+    console.log(`a user connected[id:${ socket.id }]`)
+    socket.on('POST_MESSAGE', function(data) {
+      console.log(`posted[name:${ data.name },message:${ data.message }]`)
+      io.sockets.emit('MESSAGE', data)
+    });
+  })
 };
 
 //export
